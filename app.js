3190af6f50dafd0163b7bc5b0f1528d6b21b2400
Martín del Rosario <delrosariomartin23@gmail.com>
2021-08-02 15:02:01 -0300

    agrego el backend actualizado del logueo

--- app.js
+++ app.js
@@ -6,20 +6,13 @@ loginForm.addEventListener('submit', (e) =>{
     e.preventDefault();
     const email = document.querySelector('#idemail').value;
     const pass = document.querySelector('#idpass').value;
-=======
-const signupForm = document.querySelector('#signup-form');
-
-signupForm.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    const email = document.querySelector('#email').value;
-    const pass = document.querySelector('#password').value;
 
 
     auth.createUserWithEmailAndPassword(email, pass)
     .catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
-      
+
       console.log(errorCode);
       console.log(errorMessage);
       // ..
