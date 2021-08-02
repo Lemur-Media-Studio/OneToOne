2fd355b58f8c04502fd5198d399248afeb2a4bea
Uriel-27 <85854805+Uriel-27@users.noreply.github.com>
2021-08-02 15:02:58 -0300

    Update app.js

--- app.js
+++ app.js
@@ -6,14 +6,6 @@ loginForm.addEventListener('submit', (e) =>{
     e.preventDefault();
     const email = document.querySelector('#idemail').value;
     const pass = document.querySelector('#idpass').value;
-=======
-const signupForm = document.querySelector('#signup-form');
-
-signupForm.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    const email = document.querySelector('#email').value;
-    const pass = document.querySelector('#password').value;
-
 
     auth.createUserWithEmailAndPassword(email, pass)
     .catch(function(error) {
