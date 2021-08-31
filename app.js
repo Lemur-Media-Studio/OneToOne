c6c6a283c22f3961fb5f13b733866b7594c9d19d
Facundo Ruiz Perez <80124560+facumruiz@users.noreply.github.com>
2021-08-31 12:32:42 -0300

    Agrego parte de back actualizada

--- app.js
+++ app.js
@@ -1,21 +1,58 @@
-console.log('CONEXION CON BACK HECHA')
+  
+/*  DOCUMENTACION DE FIREBASE: https://firebase.google.com/docs/auth/web/start
+    LINK DE BASE DE DATOS: https://console.firebase.google.com/u/1/project/inicio-logueo-one/overview
+***************************************************************************************************************
+***************************************************************************************************************
+    DECLARO UNA FUNCION PARA MANDAR DATOS A BASE DE DATOS   */
 
-const loginForm = document.querySelector('#login-form');
+const db = firebase.firestore();
 
-loginForm.addEventListener('submit', (e) =>{
+const todoForm = document.getElementById('todo_form');
+
+const create = (name, producto, id, description) => {
+    db.collection('Usuarios').doc(name).set({
+
+        id,
+        name,
+        producto
+    })
+
+    db.collection('Productos').doc(producto).set({
+        name,
+        producto,
+        description
+    })
+
+}
+
+todoForm.addEventListener('submit', async e => {
     e.preventDefault();
-    const email = document.querySelector('#idemail').value;
-    const pass = document.querySelector('#idpass').value;
+    const name = todoForm['todo_name'].value;
+    const id = todoForm['todo_id'].value;
+    const producto = todoForm['todo_url'].value;
+    const description = todoForm['todo_description'].value;
+
+    await create(name, id, producto, description); // Llamo a mi función create
+
+    todoForm.reset(); // Reseteamos los campos
+});
+
+//Funcion para crear usuarios con Email y Password 
+function funcion(){
+    var email = document.getElementById('email').value;
+    var password = document.getElementById('email').value; // ? -> chequear esta linea
 
 
-    auth.createUserWithEmailAndPassword(email, pass)
+    firebase.auth().createUserWithEmailAndPassword(email, password)
     .catch(function(error) {
       var errorCode = error.code;
       var errorMessage = error.message;
-
+      
       console.log(errorCode);
       console.log(errorMessage);
       // ..
     });
 
-})
+
+
+}
