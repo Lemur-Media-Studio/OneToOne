b829d1549196d1fbbb1c0dfa096666b5faaa3464
Uriel Mendizabal <uri.mendizabal@gmail.com>
2021-10-12 17:49:36 -0300

    agrego al parte de back

--- scripts/admin.js
+++ scripts/admin.js
@@ -5,33 +5,62 @@
 ***************************************************************************************************************
     DECLARO UNA FUNCION PARA MANDAR DATOS A BASE DE DATOS   */
 
-const db = firebase.firestore();
-const todoForm = document.getElementById('todo_form');
+    const db = firebase.firestore();
+    const todoForm = document.getElementById('todo_form');
+    const taskContainer = document.getElementById('cont');
+    const taskContainer2 = document.getElementById('cont2');
+    const taskContainer3 = document.getElementById('cont3');
+    const taskContainer4 = document.getElementById('cont4');
+    const taskProdu = document.getElementById('prod1');
+    const taskProdu2 = document.getElementById('prod2');
+    const taskContainer5 = document.getElementById('cont5');
 
-const create = (name, producto, id, description) => {
-    db.collection('Usuarios').doc(name).set({
-        id,
-        name,
-        producto
+    const create = (name, producto, id, description) => {
+        db.collection('Usuarios').doc(name).set({
+            id,
+            name,
+            producto
+        })
+    
+        db.collection('Productos').doc(producto).set({
+            name,
+            producto,
+            description
+        })
+    
+    }
+    
+    const listar = () => db.collection('Usuarios').get();
+    window.addEventListener('DOMContentLoaded', async (e) => {
+        e.preventDefault();
+        const querySnapshot = await listar();
+        querySnapshot.forEach(doc => {
+            console.log(doc.data())
+            taskContainer.innerHTML += `<div>${doc.data().name}</div>`
+            taskContainer2.innerHTML += `<div>${doc.data().id}</div>`
+            taskContainer3.innerHTML += `<div>${doc.data().producto}</div>`
+            taskContainer4.innerHTML += `<div class="col btn-group"><button class="btn bg-success px-2" style="color: #fff;">UID</button>
+            <button class="btn bg-danger px-2"><i class="fas fa-trash-alt" style="color: #fff;"></i></button>
+            <button class="btn bg-warning px-2" style="color: #fff;">EDITAR</button> </div>`
+            taskProdu.innerHTML += `<div>${doc.data().producto}</div>`
+            taskProdu2.innerHTML += `<div>${doc.data().description}</div>`
+            taskContainer5.innerHTML += `<div class="col btn-group"><button class="btn bg-success px-2" style="color: #fff;">UID</button>
+            <button class="btn bg-info px-2" style="color: #fff;">FID</button>
+            <button class="btn bg-danger px-2"><i class="fas fa-trash-alt" style="color: #fff;"></i></button>
+            <button class="btn bg-warning px-2" style="color: #fff;">EDITAR</button> </div>`
+        })
     })
+    
 
-    db.collection('Productos').doc(producto).set({
-        name,
-        producto,
-        description
-    })
-
-}
-
-todoForm.addEventListener('submit', async e => {
-    e.preventDefault();
-    const name = todoForm['todo_name'].value;
-    const id = todoForm['todo_id'].value;
-    const producto = todoForm['todo_url'].value;
-    const description = todoForm['todo_description'].value;
-
-    await create(name, id, producto, description); // Llamo a mi función create
-
-    todoForm.reset(); // Reseteamos los campos
-});
+    
+    todoForm.addEventListener('submit', async e => {
+        e.preventDefault();
+        const name = todoForm['todo_name'].value;
+        const id = todoForm['todo_id'].value;
+        const producto = todoForm['todo_url'].value;
+        const description = todoForm['todo_description'].value;
+    
+        await create(name, id, producto, description); // Llamo a mi función create
+        todoForm.reset(); // Reseteamos los campos
+    });
 
--- scripts/login.js
+++ scripts/login.js
@@ -1,22 +1,72 @@
-const sigupForm = document.querySelector('#login-form');
+/*  DOCUMENTACION DE FIREBASE: https://firebase.google.com/docs/auth/web/start
+    LINK DE BASE DE DATOS: https://console.firebase.google.com/u/1/project/inicio-logueo-one/overview
+***************************************************************************************************************
+***************************************************************************************************************
+    DECLARO UNA FUNCION PARA MANDAR DATOS A BASE DE DATOS   */
 
-sigupForm.addEventListener('submit', (e) =>{
-    e.preventDefault();
+    const sigupForm = document.querySelector('#login-form');
 
 
-    const email = document.querySelector('#idemail').value;
-    const password = document.querySelector('#idpass').value;
 
-
-    auth.createUserWithEmailAndPassword(email, password)
-    .then((userCredential) => {
-      // Signed in
-      var user = userCredential.user;
-      // ...
+    //SINUP
+    sigupForm.addEventListener('submit', (e) =>{
+        e.preventDefault();
+    
+    
+        const email = document.querySelector('#idemail').value;
+        const password = document.querySelector('#idpass').value;
+    
+    
+        auth.createUserWithEmailAndPassword(email, password)
+        .then((userCredential) => {
+          // Signed in
+          console.log('Logeado')
+          $('#exampleModal2').modal('hide')
+          const user = userCredential.user;
+          sigupForm.reset();
+          window.alert("USUARIO REGISTRADO CON EXITO") 
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode)
+          console.log(errorMessage)
+          window.alert(errorMessage)
+          // .. 
+        });
+    
     })
-    .catch((error) => {
-      var errorCode = error.code;
-      var errorMessage = error.message;
-      // ..
-    });
-})
+    
+    
+    
+    
+    //SININ
+    const signinForm = document.querySelector('#signin-form');
+    
+    signinForm.addEventListener('submit', e => {
+      e.preventDefault();
+      const email = document.querySelector('#loginemail').value;
+      const password = document.querySelector('#loginpass').value;
+    
+      auth.signInWithEmailAndPassword(email,password)
+        .then(userCredential => {
+          signinForm.reset();
+          $('#exampleModal').modal('hide')
+          console.log('sign in')
+          window.location = 'productos.html'
+    
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode)
+          console.log(errorMessage)
+          window.alert(errorMessage)
+          
+          // .. 
+        });
+        signinForm.reset();
+      })
+    
+    
+    
