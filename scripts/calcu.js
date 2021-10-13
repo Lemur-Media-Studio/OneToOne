df888fb60f67c30bd02e738bfcd8c85ec6c6d73e
Uriel Mendizabal <uri.mendizabal@gmail.com>
2021-10-13 00:39:44 -0300

    agregamos el modal con la info de la calculadora

--- scripts/calcu.js
+++ scripts/calcu.js
@@ -1,3 +1,7 @@
+const pf = document.getElementById('productofinal');
+const pe = document.getElementById('productoenvio');
+
+
 
 function calculo(valor, precio, x, xt, preciokg){
     var valor = document.getElementById('idvalor').value;
@@ -9,9 +13,14 @@ function calculo(valor, precio, x, xt, preciokg){
     console.log(precio);
     x = (preciokg)*(precio);
     console.log(x);
-    window.alert("El precio del envio es: $" + (x));
+    //window.alert("El precio del envio es: $" + (x));
     xt = parseFloat(valor) + parseFloat(x);
     console.log(xt);
-    window.alert("El precio final (con envio): $" + (xt));
+    //window.alert("El precio final (con envio): $" + (xt));
+
+    pe.innerHTML = `<div><strong>El precio del envio es: USD ${x}</strong></div>`
+
+    pf.innerHTML = `<div><strong>El precio final del producto es: USD ${xt}</strong></div>`
+
 
 }
