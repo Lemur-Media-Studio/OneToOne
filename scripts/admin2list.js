27afe54ceab7fdca5453841d5eb3df11aa9dced3
Uriel Mendizabal <uri.mendizabal@gmail.com>
2021-11-07 18:48:35 -0300

    agregamos estados en el tracking

--- scripts/admin2list.js
+++ scripts/admin2list.js
@@ -11,13 +11,13 @@ auth.onAuthStateChanged(user => {
   if (user) {
     console.log('user logged in: ');
     const userdata = db.collection('users').doc(user.uid).get().then(doc => {
-      const data = db.collection("Productos").get().then(snapshot => {
+      const data = db.collection('tasks').get().then(snapshot => {
 
         console.log(doc.data().apellido)
         console.log(doc.data().fecha)
         console.log(doc.data().email)
-        const idu = (doc.data().UID)
-        console.log(idu);
+        const uid = (doc.data().UID)
+        console.log(uid);
         cositas(snapshot.docs)
 
 
@@ -32,36 +32,16 @@ auth.onAuthStateChanged(user => {
       })
 
     });
-    /*
-            const produdata =db.collection('Productos').doc(user.uid).get().then(doc=>{
-                console.log(doc.data().producto)//marca
-                console.log(doc.data().name)//producto
-                //console.log(doc.data().uid)
-                console.log(doc.data().id)//precio
-                console.log(doc.data().description)//traking id
 
-                taskNamepro.innerHTML += `<div class="card-title producto-card">${doc.data().name}</div>`;    
-                taskPrecio.innerHTML += `<div class="card-title producto-card">$ ${doc.data().id}</div>`;   
-                taskMarca.innerHTML += `<div class="card-title producto-card">${doc.data().producto}</div>`;
-                taskEstado.innerHTML += `<div class="titulo-trayecto"><strong>EN CAMINO</strong></div>`;
-                taskqr.innerHTML += `<img src="img/qrcode.png" class="img-qr img-fluid rounded-start" alt="código QR">`;
-            });*/
 
     const cositas = (data) => {
       data.forEach(doc => {
-        const guide = doc.data();
+        const guide = doc.data().uid;
+        console.log(guide)
 
         if (doc.data().uid == user.uid) {
           console.log(guide);
-          /*
-          taskNamepro.innerHTML += `<div class="card-title producto-card">${doc.data().name}</div>`;    
-          taskPrecio.innerHTML += `<div class="card-title producto-card">$ ${doc.data().id}</div>`;   
-          taskMarca.innerHTML += `<div class="card-title producto-card">${doc.data().producto}</div>`;
-          taskEstado.innerHTML += `<div class="titulo-trayecto"><strong>EN CAMINO</strong></div>`;
-          taskbot1.innerHTML += `<button class="btn btn-dark boton-azul-productos" type="button">Ver factura</button>
-          <button class="btn boton-beige-productos" type="button">Ayuda</button>`;          
-          taskqr.innerHTML += `<img src="img/qrcode.png" class="img-qr img-fluid rounded-start" alt="código QR">`;
-          */
+
           card.innerHTML += `<div class="card mb-3 card-productos shadow p-3 mb-5 bg-body rounded" style="max-width: 1100px;">
             <div class="row g-0 align-items-center">
             <div class="col-md-2 linea-blanco-right">
@@ -69,13 +49,13 @@ auth.onAuthStateChanged(user => {
             </div>
             <div class="col">
               <div class="card-body">
-                <h6 class="card-title producto-card">${doc.data().name}</h6>
-                <p class="card-text producto-card">${doc.data().producto}</p>
+                <h6 class="card-title producto-card">${doc.data().producto}</h6>
+                <p class="card-text producto-card">${doc.data().marca}</p>
               </div>
             </div>
             <div class="col">
               <div class="card-body">
-                <h6 class="card-title producto-card">$ ${doc.data().id}</h6>
+                <h6 class="card-title producto-card">$ ${doc.data().valor}</h6>
               </div>
             </div>
             <div class="col">
